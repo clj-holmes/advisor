da30eecf0065a8fa538ea63b7a92f4afc2dfb625
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-09-22 21:45:05 -0300

    add versions and clj-holmes detection

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const packageInformationDiv = document.getElementById("package-information");
-const packageInformation = fetch("../package.json")
+if(packageInformationDiv){
+  const packageInformation = fetch("../package.json")
   .then(r => r.json())
   .then(package => {
     dependencyName = `<h1 class="text-6xl"><a href="../" >${package.name}</href></h1>`
     packageInformationDiv.insertAdjacentHTML('beforeend', dependencyName)
   })
+}
 
 const vulnerableDependenciesDiv = document.getElementById("vulnerable-dependencies");
-const vulnerableDependencies = fetch("clj-watson.json")
+if (vulnerableDependenciesDiv){
+  const vulnerableDependencies = fetch("clj-watson.json")
   .then(r => r.json())
   .then(vulnerableDependencies => {
     vulnerableDependencies.map(vulnerableDependency => {
-      
+
       cves = vulnerableDependency.vulnerabilities.map(vulnerability => vulnerability.advisory.identifiers[0].value)
 
-    console.log(cves)
-      vulnerableDependencyHTML = `
+      const vulnerableDependencyHTML = `
 <div class="border-solid border-4 border-sky-1000 p-3 mr-3 mb-3 shadow-md">
 <p class="text-2xl">${vulnerableDependency.dependency} ${vulnerableDependency["mvn/version"]}</p>
 <b>Secure Version:</b>
@@ -28,6 +30,36 @@ const vulnerableDependencies = fetch("clj-watson.json")
 <p>${cves}</p>
 </div>
 `
-    vulnerableDependenciesDiv.insertAdjacentHTML('beforeend', vulnerableDependencyHTML);
+      vulnerableDependenciesDiv.insertAdjacentHTML('beforeend', vulnerableDependencyHTML);
+    })
   })
-})
+}
+
+const vulnerableCodeDiv = document.getElementById("vulnerable-code");
+if (vulnerableCodeDiv){
+  const vulnerableCode = fetch("clj-holmes.json")
+  .then(r => r.json())
+  .then(vulnerabilities => {
+    vulnerabilities.map(vulnerability => {
+      const vulnerabilityDescription = `
+<p class="text-2xl">${vulnerability.filename}<p>
+<p>${vulnerability.name}<p>
+<p>${vulnerability.message}<p>
+`
+      vulnerableCodeDiv.insertAdjacentHTML('beforeend', vulnerabilityDescription);
+
+      vulnerability.findings.map(finding => {
+        const findingHTML = `
+<div class="border-solid border-4 border-sky-1000 p-3 mr-3 mb-3 shadow-md">
+<b>Parent:</b><p> ${finding.parent}</p>
+<code class="bg-slate-500	text-white">
+${finding.row}| ${finding.code}
+</code>
+<b>
+</div>
+`
+        vulnerableCodeDiv.insertAdjacentHTML('beforeend', findingHTML);
+      })
+    })
+  })
+}
